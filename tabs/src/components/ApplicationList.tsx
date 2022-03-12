aed4a2024a304e75e9f7aab46dc7466d0459a4ab
Daniel Watford <daniel@watfordconsulting.com>
2022-03-12 10:10:25 +0000

    Display basic table of applications with placeholder photo (#1)

--- tabs/src/components/ApplicationList.tsx
+++ tabs/src/components/ApplicationList.tsx
@@ -1,35 +1,48 @@
-import { List, Image, ListItemProps } from "@fluentui/react-northstar";
+import { Image, Table, TableRowProps } from "@fluentui/react-northstar";
 import { useMemo } from "react";
 import { useApplications } from "./contexts/applications-context";
 
 const ApplicationList = () => {
   const { applications } = useApplications();
 
-  const items: ListItemProps[] = useMemo(() => {
+  const header = {
+    items: [
+      "Photo",
+      "Name",
+      "Address",
+      "Phone",
+      "Email",
+      "Requested teams",
+      "Availability",
+      "Options",
+    ],
+  };
+
+  const tableItems: TableRowProps[] = useMemo(() => {
     return applications.map((application, index) => {
       return {
-        key: index,
-        media: (
+        items: [
           <Image
             height="256px"
             width="256px"
             src="https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/RobertTolbert.jpg"
             avatar
-          />
-        ),
-        header: application.title,
-        headerMedia: "5:22:40 PM",
-        content: (
-          <div>
-            <h1>H1 PLACEHOLDER</h1>
-            <p>PARAGRAPH PARAGRAPH</p>
-          </div>
-        ),
+          />,
+          application.title,
+          application.address,
+          { content: application.telephone, truncateContent: true },
+          "",
+          `${application.teamPreference1 ?? ""} / ${
+            application.teamPreference2 ?? ""
+          } / ${application.teamPreference3 ?? ""}`,
+          "",
+          "",
+        ],
       };
     });
   }, [applications]);
 
-  return <List selectable items={items} />;
+  return <Table header={header} rows={tableItems} />;
 };
 
 export default ApplicationList;
